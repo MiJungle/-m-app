3fc87a39b94abbfaddcb5c3e3fd4b9351bef23b5
Mijungle <mijunglee@MiJungs-MacBook-Pro.local>
2023-02-09 15:38:29 +0900

    add kakao api

--- api.js
+++ api.js
@@ -4,3 +4,19 @@ export const add = (numbers) => {
     }, 0);
 }
 
+export function getData(){
+    $.ajax({
+    method: "GET",
+    url: "https://dapi.kakao.com/v3/search/book?target=title",
+    data: {
+        query: "호모사피엔스",
+    },
+    headers: {
+        Authorization: "KakaoAK 37b97e7c16e1ed8a39ab8f1a96fc4360"
+    },
+})
+    .done(function (msg) {
+        console.log(msg);
+    });
+return
+}
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-import { add } from './api.js';
+import { add, getData } from './api.js';
 
 console.log(add([1, 2, 3,4]))
+console.log(getData())
